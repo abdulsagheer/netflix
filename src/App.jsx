77aee00e5083362dcbca875021733a52b0a0aad4
abdulsagheer <abdulsagheeras29@gmail.com>
2022-06-26 17:41:50 +0530

    Setting up project for Netflix

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import './app.scss'
+import Hello from './pages/Hello/Hello';
+
+const App = () => {
+  return <Hello />
+};
+
+export default App;
---
+++ src/pages/Hello/Hello.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AcUnitIcon from "@mui/icons-material/AcUnit";
+
+const Hello = () => {
+  return (
+    <div className="home">
+      <AcUnitIcon />
+    </div>
+  );
+}
+
+export default Hello
