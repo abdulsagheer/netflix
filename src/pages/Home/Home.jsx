89f65c396a7804e948aaf566a1e9079adb0ecf88
abdulsagheer <abdulsagheeras29@gmail.com>
2022-06-26 23:41:14 +0530

    Added list item

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
 import Featured from '../../components/Featured/Featured';
 import Navbar from '../../components/Navbar/Navbar'
+import List from '../../components/List/List';
 
 
-const Hello = () => {
+const Home = () => {
   return (
     <div className="home">
       <Navbar />
-      <Featured type="movie"/>
+      <Featured type="movie" />
+      <List />
+      <List />
+      <List />
+      <List />
     </div>
   );
 }
 
-export default Hello
+export default Home
